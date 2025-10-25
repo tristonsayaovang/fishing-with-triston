dae954f816cc12a591b1ad8856df20e1db28a48d
Triston Sayaovang <tristonsayaovang@gmail.co>
2025-10-25 03:21:48 -0500

    start of home hero

---
+++ web/themes/custom/fishing_with_triston/js/mobile-menu.js
@@ -0,0 +1,46 @@
+(function (w) {
+    'use strict';
+
+    const
+        document = w.document,
+        console = w.console,
+        run = () => {
+
+            const header = document.querySelector('[data-role~=siteHeader]');
+
+            if (!header) {
+                return;
+            }
+
+            const mobileNav = header.querySelector('[data-role~=mobileNav]');
+            const mobileNavList = header.querySelector('[data-role~=mobileNavList]');
+            const primaryNav = header.querySelector('#block-fishing-with-triston-main-menu');
+            const mobileMenuButton = header.querySelector('[data-role~=mobileMenuButton]');
+            const dialog = header.querySelector('[data-role~=mobileNavDialog]');
+
+
+            primaryNav.querySelectorAll('li').forEach((link) => {
+                const clonedLink = link.cloneNode(true);
+                clonedLink.classList.add('mobile-navigation-link');
+                mobileNavList.appendChild(clonedLink);
+            });
+
+
+            mobileMenuButton.addEventListener('click', () => {
+                dialog.showModal();
+            });
+
+        },
+
+        init = () => {
+            if (['complete', 'interactive'].includes(document.readyState)) {
+                run();
+            } else {
+                document.addEventListener('DOMContentLoaded', run);
+            }
+        };
+
+    init();
+})
+
+    (window);
